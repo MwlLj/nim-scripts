4ce80b7d13cc237f9a2f5280ffe5bd4717c1e49d
MwlLj <731025894@qq.com>
2020-03-25 20:52:35 +0800

    新增根据regex删除的模式

---
+++ delete_rule_files/src/delete_by_regex/delete.nim
@@ -0,0 +1,17 @@
+import "path/vertical_walk" as walk
+
+import os
+import re
+import strformat
+
+proc delete*(root: string, match: string) =
+    #[ 
+    ## 遍历目录
+    ]#
+    proc iter(parent: string, name: string, path: string): walk.CbResult =
+        let (dir, parentName, ext) = os.splitFile(parent)
+        if name.contains(re(fmt"{match}")):
+            echo(1111)
+            os.removeFile(path)
+        return walk.CbResult.Continue
+    walk.walk(root, iter)
--- delete_rule_files/src/delete_rule_files.nim
+++ delete_rule_files/src/delete_rule_files.nim
@@ -1,4 +1,5 @@
-import "delete_arm_64_lib/delete"
+import "delete_arm_64_lib/delete" as arm64lib_delete
+import "delete_by_regex/delete" as byregex_delete
 
 import "cmd/go_style"
 
@@ -6,10 +7,16 @@ proc main() =
     var cmdHandler = go_style.newCmd()
     let mode = cmdHandler.registerWithDesc("-mode", "delete-arm-64-lib", "select feature")
     let root = cmdHandler.registerWithDesc("-root", ".", "root")
+    let match = cmdHandler.registerWithDesc("-match", "", "regex rule")
     cmdHandler.parse()
     case mode[]
     of "delete-arm-64-lib":
-        delete.delete(root[])
+        arm64lib_delete.delete(root[])
+    of "delete-by-regex":
+        if match[] == "":
+            echo("match is empty")
+            return
+        byregex_delete.delete(root[], match[])
 
 when isMainModule:
     main()
